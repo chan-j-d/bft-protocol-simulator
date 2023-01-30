dae71e9a181d85b72114225a21fb9494452c9c77
chan-j-d <chan.j.d.98@gmail.com>
2023-01-31 00:09:58 +0800

    Add snapshot to output

--- src/main/java/simulation/simulator/Simulator.java
+++ src/main/java/simulation/simulator/Simulator.java
@@ -11,19 +11,42 @@ import java.util.PriorityQueue;
 public class Simulator {
 
     private PriorityQueue<Event> eventQueue;
+    private int roundCount;
+    private List<NetworkNode> nodes;
+    private static int snapshotInterval = 10;
     public Simulator(List<NetworkNode> nodes) {
+        this.nodes = nodes;
         eventQueue = new PriorityQueue<>();
         for (NetworkNode node : nodes) {
             eventQueue.add(new InitializationEvent(node));
             eventQueue.add(new TimedEvent(node.getNextNotificationTime(), node));
         }
+
+        roundCount = 0;
     }
 
     public String simulate() {
         Event nextEvent = eventQueue.poll();
         List<Event> resultingEvents = nextEvent.simulate();
         eventQueue.addAll(resultingEvents);
-        return nextEvent.toString();
+        roundCount++;
+
+        String finalString = nextEvent.toString();
+        if (roundCount % snapshotInterval == 0) {
+            finalString = finalString + "\n\nSnapshot:\n" + getSnapshotOfNodes() + "\n" + eventQueue + "\n";
+        }
+        return finalString;
+    }
+
+    private String getSnapshotOfNodes() {
+        if (nodes.isEmpty()) {
+            return "";
+        } else {
+            return nodes.stream()
+                    .map(NetworkNode::toString)
+                    .reduce((x, y) -> x + "\n" + y)
+                    .get();
+        }
     }
 
     public boolean isSimulationOver() {
