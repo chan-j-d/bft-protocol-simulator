b0fccc350357aeb173328ec1f26718d16f5b94d1
chan-j-d <chan.j.d.98@gmail.com>
2023-02-28 00:11:45 +0800

    Remove NetworkUtil and reimplement clique method

--- src/main/java/simulation/network/topology/NetworkTopology.java
+++ src/main/java/simulation/network/topology/NetworkTopology.java
@@ -1,22 +1,24 @@
 package simulation.network.topology;
 
-import simulation.network.NetworkUtil;
+import simulation.network.entity.EndpointNode;
 import simulation.network.entity.Node;
+import simulation.network.router.Switch;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NetworkTopology {
-    public static <T> List<Node<T>> arrangeCliqueStructure(List<? extends Node<T>> nodes) {
-        for (Node<T> node : nodes) {
-            node.clearNeighbors();
-            List<Node<T>> copy = new ArrayList<>(nodes);
-            copy.remove(node);
-            node.addNeighbors(copy);
-
-            copy.forEach(neighbor -> node.registerDestination(neighbor, neighbor));
+    public static <T> List<Node<T>> arrangeCliqueStructure(List<? extends EndpointNode<T>> nodes) {
+        List<Switch<T>> switches = new ArrayList<>();
+        for (EndpointNode<T> node : nodes) {
+            Switch<T> switch_ = new Switch<>("Switch-" + node.getName(), new ArrayList<>(nodes));
+            switches.add(switch_);
+            node.setOutflowNodes(List.of(switch_));
         }
-        NetworkUtil.updateDestinationTables(nodes);
+
+        for (Switch<T> switch_ : switches) {
+            switch_.setNeighbors(new ArrayList<>(switches));
+        }
         return new ArrayList<>(nodes);
     }
 }
