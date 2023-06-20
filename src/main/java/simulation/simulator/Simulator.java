f319777fe410238d330f953caa520ea8249dfccd
chan-j-d <chan.j.d.98@gmail.com>
2023-06-20 21:37:47 +0800

    Add javadocs to simulation.simulator package

--- src/main/java/simulation/simulator/Simulator.java
+++ src/main/java/simulation/simulator/Simulator.java
@@ -2,10 +2,28 @@ package simulation.simulator;
 
 import java.util.Optional;
 
+/***
+ * Simulator interface that runs a BFT protocol simulation.
+ */
 public interface Simulator {
 
+    /**
+     * Simulates one event and returns an optional String to print.
+     */
     Optional<String> simulate();
+
+    /**
+     * Returns a String representation of a view of the nodes in the simulation.
+     */
     String getSnapshotOfNodes();
+
+    /**
+     * Returns true if the simulation is over.
+     */
     boolean isSimulationOver();
+
+    /**
+     * Returns the results of the BFT protocol simulation run.
+     */
     RunResults getRunResults();
 }
