102638976d518ee6af13f3ef0bd79494a67a204d
chan-j-d <chan.j.d.98@gmail.com>
2023-07-23 02:00:27 +0800

    Update json reading to use staggered json format

--- py/run_config_json.py
+++ py/run_config_json.py
@@ -1,12 +1,14 @@
 import json
 
 
-def create_run_config_json(num_runs: int, num_consensus: int, starting_seed: int,
-                           seed_multiplier: int, num_nodes: int, node_processing_rate: float,
-                           switch_processing_rate: float, base_time_limit: float, network_type: str,
-                           validator_type: str, network_parameters: list):
-    json_dic = {"numRuns": num_runs, "numConsensus": num_consensus, "startingSeed": starting_seed,
-                "seedMultiplier": seed_multiplier, "numNodes": num_nodes, "nodeProcessingRate": node_processing_rate,
-                "switchProcessingRate": switch_processing_rate, "baseTimeLimit": base_time_limit,
-                "networkType": network_type, "validatorType": validator_type, "networkParameters": network_parameters}
+def create_run_config_json(num_runs: int, starting_seed: int, seed_multiplier: int, 
+                           num_nodes: int, num_consensus: int, base_time_limit: float, node_processing_rate: float, consensus_protocol: str,
+                           switch_processing_rate: float, message_channel_success_rate: float, 
+                           network_type: str, network_parameters: list):
+    switch_settings_dic = {"switchProcessingRate": switch_processing_rate, "messageChannelSuccessRate": message_channel_success_rate}
+    network_settings_dic = {"switchSettings": switch_settings_dic, "networkType": network_type, "networkParamters": network_parameters}
+    validator_settings_dic = {"numNodes": num_nodes, "numConsensus": num_consensus, "baseTimeLimit": base_time_limit, 
+                              "nodeProcessingRate": node_processing_rate, "consensusProtocol": consensus_protocol}
+    json_dic = {"numRuns": num_runs,"startingSeed": starting_seed, "seedMultiplier": seed_multiplier,
+                "validatorSettings": validator_settings_dic, "networkSettings": network_settings_dic}
     return json.dumps(json_dic, indent=4)
